8573cabd34cc5007badc0b378913f45404c30aa2
Swatantra <swattu@gmail.com>
2014-11-26 19:27:36 +0530

    Added PPH of Spring

---
+++ core/src/main/java/org/swat/core/spring/PropertyPlaceHolder.java
@@ -0,0 +1,56 @@
+package org.swat.core.spring;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by swat
+ * on 25/11/14.
+ */
+public class PropertyPlaceHolder extends PropertyPlaceholderConfigurer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertyPlaceHolder.class);
+    public static final String CONFIG_VARIABLE = "ENV_CONF";
+
+    public PropertyPlaceHolder() throws Exception {
+        setSystemPropertiesMode(SYSTEM_PROPERTIES_MODE_OVERRIDE);
+        setSearchSystemEnvironment(true);
+        Properties props = new Properties();
+
+        String confFile = getProperty(CONFIG_VARIABLE, props, "dev.properties");
+
+        File file = new File(confFile);
+        if (!file.exists()) {
+            LOGGER.error("Runtime file does not exist : " + confFile);
+            throw new Exception("Runtime file does not exist : " + confFile);
+        }
+        List<Resource> resources = new ArrayList<>();
+        resources.add(new FileSystemResource(file));
+        LOGGER.info("Environment configuration file is: " + confFile);
+
+        setLocations(resources.toArray(new Resource[resources.size()]));
+    }
+
+    public String getProperty(String key, Properties properties, String defaultValue) {
+        String value = System.getProperty(key);
+        if (StringUtils.isBlank(value)) {
+            value = System.getenv(key);
+        }
+        if (StringUtils.isBlank(value) && properties != null) {
+            value = properties.getProperty(key);
+        }
+        if (StringUtils.isBlank(value)) {
+            value = defaultValue;
+        }
+        return value;
+    }
+
+}
