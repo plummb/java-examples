4f3e0e139050d7df61547da2d0ba28d58e7d1852
Swatantra <swattu@gmail.com>
2017-06-12 19:36:38 +0530

    Added dependency for Neo4J

---
+++ neo4j/src/main/java/org/swat/neo4j/Person.java
@@ -0,0 +1,45 @@
+package org.swat.neo4j;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.List;
+
+/**
+ * Created by swat
+ * on 12/6/17.
+ */
+@NodeEntity
+public class Person {
+    @GraphId
+    private String id;
+    private String name;
+
+    @Relationship(type="ACTS_IN", direction = Relationship.INCOMING)
+    private List<Person> children;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+}
