d305a3e27f39ddd5487198cc7ac36db5523e9737
Swatantra Agrawal <swattu@gmail.com>
2017-10-09 04:17:15 +0000

    PersonServiceImpl.java created online with Bitbucket

---
+++ mongodb/src/main/java/org/swat/mongodb/PersonServiceImpl.java
@@ -0,0 +1,26 @@
+package org.swat.mongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Created by swat
+ * on 11/6/17.
+ */
+@Service
+public class PersonServiceImpl {
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person insert(Person person) {
+        person.setId(UUID.randomUUID().toString());
+        personRepository.insert(person);
+        return person;
+    }
+
+    public Person findById(String id) {
+        return personRepository.findOne(id);
+    }
+}
