9096e428c4728d07f12b4cc2a334350a856ae1b9
Swat <swatantra.agrawal@capillarytech.com>
2018-03-07 17:07:21 +0530

    Rest example

---
+++ rest/src/main/java/org/swat/rest/RestMain.java
@@ -0,0 +1,11 @@
+package org.swat.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMain {
+    public static void main(String[] args) {
+        SpringApplication.run(RestMain.class, args);
+    }
+}
