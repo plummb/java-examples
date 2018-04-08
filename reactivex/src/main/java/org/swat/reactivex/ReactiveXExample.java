488c5a88571dcb811f5e517db6ec6ff87f127e62
Swat <swattu@gmail.com>
2018-04-08 15:11:07 +0530

    Updated for Threading

--- reactivex/src/main/java/org/swat/reactivex/ReactiveXExample.java
+++ reactivex/src/main/java/org/swat/reactivex/ReactiveXExample.java
@@ -2,13 +2,14 @@ package org.swat.reactivex;
 
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by swat
  * on 13/6/17.
  */
 public class ReactiveXExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Flowable.just("Hello world").subscribe(System.out::println);
         String[] words = {
                 "the",
@@ -27,6 +28,12 @@ public class ReactiveXExample {
         Observable.fromArray(words)
                 .zipWith(Observable.range(1, Integer.MAX_VALUE),
                         (string, count) -> String.format("%2d. %s", count, string))
-                .subscribe(System.out::println);
+                .observeOn(Schedulers.newThread())
+                .subscribe(s -> {
+                    Thread.sleep(1);
+                    System.out.println(s);
+                });
+        System.out.println("Hello");
+        Thread.sleep(1000);
     }
 }
